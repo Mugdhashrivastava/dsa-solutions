b789d85a63eb053fb5eed5c8fa47fe02fd19f04f
Mugdhashrivastava <84615010+Mugdhashrivastava@users.noreply.github.com>
2024-04-26 11:29:23 +0530

    Time: 10 ms (61.98%), Space: 42.2 MB (75.62%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -7,17 +7,17 @@ One to one or bi directional mapping
 reference :https://youtu.be/ogTMIFPjNkQ?feature=shared
 
 
-solution : we will make 2 hashmaps : remember a hashmap cannot have 2 same keys but they can have 2 same values to two different values 
+solution : we will make 2 hashmaps : remember a hashmap cannot have 2 same keys but they can have 2 same values to two different keys
 Basically, keys needs to be unique,values doesnt
 So this is why we will be needing 2 different hashmaps since we have one to one relationship bi-directionally
-hashmap1 = will keep all the unique keys of string s and map it to t's values so anytime when we will map s and t , if s would be repeated with differente  value of t it would exploit the rule of hashmap since key of s should be unique so we will return false if it doesnt repeat then we will continue till end
+hashmap1 = will keep all the unique keys of string s and map it to t's values so anytime when we will map s and t , if s would be repeated with differente  value of t it would exploit the rule of hashmap since key of s should be unique so if we reapeat then the second keyvalue added or newly keyvalue added will be updated and old one will be gone
 Hashmap2 = this hashmap take keys of second string t and will check if its visited by us or not during mapping ,since keys cant be repeated so if values of 't' will be repeated we will return false */
 
 
 
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        //return false if string lengths s and t are uneqal that means some characters are not mapping to anything then they can never be isomorphic
+        //return false if string lengths s and t are unequal that means some characters are not mapping to anything then they can never be isomorphic
         if(s.length() != t.length()){
             return false;
         }
@@ -29,11 +29,12 @@ class Solution {
         HashMap<Character,Boolean> visited = new HashMap<>();
         
         
+        //running a loop through string and getting the value of string s and t at each index
         for(int i=0;i<s.length();i++){
             char charS = s.charAt(i);
             char charT = t.charAt(i);
             
-            
+            //check if charS of s at current index exist in map , if it does already then check if value of charS is equal to charT ,if it doesnt match that means value has been repeated and updated thats why mapped character aint matching so return false
             if(map.containsKey(charS)){
                if(map.get(charS)!=charT){
                    return false;
@@ -42,17 +43,23 @@ class Solution {
 
             
             
-            
+           //else if it doesnt containsKey then put values in our hashmaps for the first time
             else {
+                
+                
+            //if second hashmap visited has charT already that  means we have already put the key value in our hashMap "visited" so return false otherwise continue
                 if(visited.containsKey(charT)){
                     return false;
                 }
+                
                 
                 map.put(charS,charT);
                 visited.put(charT,true);
             }
         }
         
+        
+        //if our hashmaps were able come out through these harsh test then return true
         return true;
         
     }
